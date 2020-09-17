d90afcebae8f0b57332d8615264255e5179b1c52
Jthui95 <jonathanhui.hk@gmail.com>
2020-09-16 21:08:56 -0700

    added food.js

---
+++ models/food.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+    var Food = sequelize.define("Food", {
+      name: {
+        type: DataTypes.STRING,  
+        allowNull: false,
+        validate: {
+          len: [1, 140]
+        }
+      },
+      expiration: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 140]
+      },
+        },
+
+      quantity:{
+      type: DataTypes.INTEGER,  
+      allowNull: false,
+      defaultvalue: 1,
+      validate: {
+       len: [1,10]
+      }
+    },
+
+    
+     location: {
+      type: DataTypes.STRING,  
+      allowNull: false,
+      defaultvalue: "Refrigerator", 
+      validate: {
+        len: [1, 140]
+      }
+    }
+})
+    return Food;
+  };
