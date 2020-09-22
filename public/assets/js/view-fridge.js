3629b3b2c0470148d6a44609bd6ee4bb949daa88
Maxx Sanner <maxxsanner105@gmail.com>
2020-09-21 21:03:52 -0700

    added update functionality to view js files and installed moment npm package

--- public/assets/js/view-fridge.js
+++ public/assets/js/view-fridge.js
@@ -1,12 +1,29 @@
 $(document).ready(function() {
-    var updatingFridge = false;
-
+    
     $('.delete').on("click", async function(event) {
         const deleteButtonID = $(this).attr('id');
         updatingFridge = true;
         await $.ajax({
             method: "DELETE",
             url: "/api/items/" + deleteButtonID
+        });
+        window.location.href = "view-fridge";
+    });
+
+    $('.update').on("click", async function(event) {
+        alert("Changes Saved!");
+        const buttonID = $(this).attr('id');
+        const newItem = {
+            name: $('#name-input').text().trim(),
+            quantity: $('#quantity-input').text().trim(),
+            expiration: $('#expiration-input').text().trim(),
+            description: $('#description-input').text().trim(),
+            id: buttonID
+        }
+        await $.ajax({
+            method: "PUT",
+            url: "/api/items/",
+            data: newItem
         });
         window.location.href = "view-fridge";
     });
